c736e1ae2fdadb283d5947d6aa3c5397ac1078b2
jihun-heo <karais89@gmail.com>
2019-09-14 13:27:20 +0900

    Example01 Create
    https://qiita.com/toRisouP/items/83fd28b6d4a70a7ed1d2

---
+++ Assets/Example/Example01/ObserveScript.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using UniRx;
+using UnityEngine;
+
+public class ObserveScript : MonoBehaviour
+{
+    /// <summary>
+    /// TargetCube를 묶는 GameObject
+    /// UnityEditor에서 설정해두자.
+    /// </summary>
+    public GameObject cubes;
+
+    private void Start()
+    {
+        // OnVisibleScript를 획득
+        var onVisibleScripts = cubes.GetComponentsInChildren<OnVisibleScript>();
+        
+        // Merge : 여러개의 OnVisibleObservable을 하나로 통합
+        var allOnVisibleObservable = Observable.Merge(onVisibleScripts.Select((x => x.OnVisibleObservable)));
+        
+        // 250ms 이내에 화면에 함께 찍힌 GameObject를 계산
+        allOnVisibleObservable
+            .Buffer(allOnVisibleObservable.Throttle(TimeSpan.FromMilliseconds(250)))
+            .Subscribe(x => Debug.Log(x.Count));
+    }
+}
---
+++ Assets/Example/Example01/OnVisibleScript.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+/// <summary>
+/// 게임오브젝트가 카메라에 찍힌 것을 통지하는 스크립트
+/// </summary>
+public class OnVisibleScript : MonoBehaviour
+{
+    /// <summary>
+    /// 카메라에 비친 게임오브젝트를 흐르는 스트림
+    /// </summary>
+    private Subject<GameObject> onVisibleStream = new Subject<GameObject>();
+
+    /// <summary>
+    /// 외부에 공개하는 Observable
+    /// </summary>
+    public IObservable<GameObject> OnVisibleObservable => onVisibleStream.AsObservable();
+    
+    /// <summary>
+    /// 카메라에 찍힐 때 실행되는 Unity 전용 콜백
+    /// </summary>
+    private void OnBecameVisible()
+    {
+        // OnNext에서 자신의 게임오브젝트를 스트림에 흐르게 한다.
+        onVisibleStream.OnNext(gameObject);
+    }
+}
