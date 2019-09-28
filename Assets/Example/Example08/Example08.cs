4154e214e04e7ed59ebd66cd5b1346151669ef0a
jihun-heo <karais89@gmail.com>
2019-09-28 13:01:20 +0900

    Example 08 Create

---
+++ Assets/Example/Example08/Example08.cs
@@ -0,0 +1,23 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+public class Example08 : MonoBehaviour
+{
+    private CharacterController _characterController;
+
+    private void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+        _characterController
+            .ObserveEveryValueChanged(x => x.isGrounded)
+            .Where(x => x)
+            .Subscribe(_ => Debug.Log("OnGrounded!"));
+        
+        this.UpdateAsObservable()
+            .Select(_ => _characterController.isGrounded)
+            .DistinctUntilChanged()
+            .Where(x => x)
+            .Subscribe(_ => Debug.Log("OnGrounded!"));
+    }
+}
---
+++ Assets/Example/Example08/Example08Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example08Player : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private CharacterController _characterController;
+    private Vector3 _moveDirection = Vector3.zero;
+
+    private void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        _moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        _moveDirection *= speed;
+
+        if (Input.GetButton("Jump"))
+        {
+            _moveDirection.y = jumpSpeed;
+        }
+ 
+        _moveDirection.y -= gravity * Time.deltaTime;
+
+        _characterController.Move(_moveDirection * Time.deltaTime);
+    }
+}
