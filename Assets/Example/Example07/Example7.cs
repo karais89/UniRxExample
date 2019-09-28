606a3755768c513e2a64acd83c2b2b6372e86063
jihun-heo <karais89@gmail.com>
2019-09-28 09:42:06 +0900

    exam 07 create

---
+++ Assets/Example/Example07/Example7.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UniRx;
+
+public class Example7 : MonoBehaviour
+{
+    private Animator _animator;
+
+    private StateMachineObservables _stateMachineObservables;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _stateMachineObservables = _animator.GetBehaviour<StateMachineObservables>();
+        
+        // 시작한 애니메이션의 short Name Hash 를 보여준다.
+        _stateMachineObservables
+            .OnStateEnterObservable
+            .Subscribe(stateInfo => Debug.Log(stateInfo.shortNameHash));
+
+        _stateMachineObservables
+            .OnStateEnterObservable                                               // 상태 변화를 감지
+            .Throttle(TimeSpan.FromSeconds(5))                                    // 마지막 상태 변화 후 5초 경과했을 때
+            .Where(x => x.IsName("Base Layer.Idle"))                              // 현재 재싱중인 애니메이션이 Base Layer의 Idle이라면
+            .Subscribe(_ => _animator.SetBool("Rest", true)); // Animator의 Rest 파라미터를 True로 한다.
+    }
+}
---
+++ Assets/Example/Example07/StateMachineObservables.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+public class StateMachineObservables : StateMachineBehaviour
+{
+    #region OnStateEnter
+    private Subject<AnimatorStateInfo> onStateEnterSubject = new Subject<AnimatorStateInfo>();
+
+    public IObservable<AnimatorStateInfo> OnStateEnterObservable => onStateEnterSubject;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        onStateEnterSubject.OnNext(stateInfo);
+    }
+    #endregion
+    
+    #region OnStateExit
+    private Subject<AnimatorStateInfo> onStateExitSubject = new Subject<AnimatorStateInfo>();
+
+    public IObservable<AnimatorStateInfo> OnStateExitObservable => onStateExitSubject;
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        onStateExitSubject.OnNext(stateInfo);
+    }
+    #endregion
+
+    #region OnStateMachineEnter
+    private Subject<int> onStateMachineEnterSubject = new Subject<int>();
+
+    public IObservable<int> OnStateMachineEnterObservable => onStateMachineEnterSubject;
+
+    public override void OnStateMachineEnter(Animator animator, int stateMachinePathHash)
+    {
+        onStateMachineEnterSubject.OnNext(stateMachinePathHash);
+    }
+    #endregion
+    
+    #region OnStateMachineExit
+    private Subject<int> onStateMachineExitSubject = new Subject<int>();
+
+    public IObservable<int> OnStateMachineExitObservable => onStateMachineExitSubject;
+
+    public override void OnStateMachineExit(Animator animator, int stateMachinePathHash)
+    {
+        onStateMachineExitSubject.OnNext(stateMachinePathHash);
+    }
+    #endregion
+    
+    #region OnStateMove
+    private Subject<AnimatorStateInfo> onStateMoveSubject = new Subject<AnimatorStateInfo>();
+
+    public IObservable<AnimatorStateInfo> OnStateMoveObservable => onStateMoveSubject;
+
+    public override void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        onStateMoveSubject.OnNext(stateInfo);
+    }
+    #endregion
+    
+    #region OnStateIK
+    private Subject<AnimatorStateInfo> onStateIKSubject = new Subject<AnimatorStateInfo>();
+
+    public IObservable<AnimatorStateInfo> OnStateIKObservable => onStateIKSubject;
+
+    public override void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        onStateIKSubject.OnNext(stateInfo);
+    }
+    #endregion
+}
