3c6351112e6661baeb4813d37fcea43ab8c826cf
jihun-heo <karais89@gmail.com>
2019-09-28 13:31:41 +0900

    Example09 Create

---
+++ Assets/Example/Example09/ObservableLifeTime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+public class ObservableLifeTime : MonoBehaviour
+{
+    private void Start()
+    {
+        // 1 초마다 메시지를 발행하는 Observable
+        Observable.Timer(TimeSpan.FromSeconds(0), TimeSpan.FromSeconds(1))
+            .Subscribe(x => Debug.Log(x))
+            .AddTo(gameObject); // GameObject의 수명과 연결.
+
+        // 3 초 후에 GameObject를 제거한다
+        Invoke("DestroyGameObject", 3);
+    }
+
+    /// <summary>
+    /// 로그를 출력하고 오브젝트를 제거한다.
+    /// </summary>
+    private void DestroyGameObject()
+    {
+        Debug.Log("Destroy");
+        Destroy(gameObject);
+    }
+}
