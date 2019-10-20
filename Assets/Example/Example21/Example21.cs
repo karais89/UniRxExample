7308b4d06dddee602a5378f65222617a0e7b520e
karais89 <karais89@gmail.com>
2019-10-20 23:19:46 +0900

    exam21 create

---
+++ Assets/Example/Example21/Example21.cs
@@ -0,0 +1,20 @@
+﻿using UniRx;
+using UnityEngine;
+
+public class Example21 : MonoBehaviour
+{    
+    private void Start()
+    {
+        // Subject 작성
+        Subject<string> subject = new Subject<string>();
+
+        // 3회 Subscrbie
+        subject.Subscribe(msg => Debug.Log("Subscribe1 : " + msg));
+        subject.Subscribe(msg => Debug.Log("Subscribe2 : " + msg));
+        subject.Subscribe(msg => Debug.Log("Subscribe3 : " + msg));
+
+        // 이벤트 메시지 발행
+        subject.OnNext("안녕하세요");
+        subject.OnNext("안녕");
+    }
+}
---
+++ Assets/Example/Example21/Example21_1.cs
@@ -0,0 +1,27 @@
+﻿using UniRx;
+using UnityEngine;
+
+public class Example21_1 : MonoBehaviour
+{
+    private void Start()
+    {
+        // 문자열을 발행하는 Subject
+        Subject<string> subject = new Subject<string>();
+
+        // 문자열을 콘솔에 출력
+        subject.Subscribe(x => Debug.Log($"플레이어가 {x}에 충돌했습니다."));
+
+        // Enemy만 필터링
+        subject
+            .Where(x => x == "Enemy") // 필터링 오퍼레이터
+            .Subscribe(x => Debug.Log($"플레이어가 {x}에 충돌했습니다."));
+
+        // 이벤트 메시지 발급
+        // 플레이어가 언급한 개체의 Tag가 발행되었다는 가정
+        subject.OnNext("Enemy");
+        subject.OnNext("Wall");
+        subject.OnNext("Wall");
+        subject.OnNext("Enemy");
+        subject.OnNext("Enemy");
+    }
+}
