aed63a2123a4599bf5d0e810689cb29508ed80c1
karais89 <karais89@gmail.com>
2019-11-03 15:50:34 +0900

    example 24 creatte

---
+++ Assets/Example/Example24/Model.cs
@@ -0,0 +1,8 @@
+﻿using UniRx;
+using UnityEngine;
+
+public class Model : MonoBehaviour
+{
+    // 외부에 공개하는 데이터
+    public ReactiveProperty<string> Name = new ReactiveProperty<string>();
+}
---
+++ Assets/Example/Example24/Presenter.cs
@@ -0,0 +1,29 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Model과 View를 연결하는 Presenter
+/// </summary>
+public class Presenter : MonoBehaviour
+{
+    // view
+    [SerializeField]
+    private InputField _nameInputField;
+
+    // model
+    [SerializeField]
+    private Model _model;
+    
+    private void Start()
+    {
+        // view가 업데이트되면 Model의 데이터를 업데이트 한다
+        _nameInputField
+            .OnValueChangedAsObservable()
+            .Subscribe(x => _model.Name.Value = x);
+
+        // model이 업데이트 되면 view를 다시 업데이트 한다.
+        _model.Name
+            .Subscribe(x => _nameInputField.text = x);
+    }
+}
