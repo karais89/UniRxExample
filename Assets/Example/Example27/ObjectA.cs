dabd8deafa959751fb7e8557b3a63b5b2d8bdd8c
karais89 <karais89@gmail.com>
2019-12-02 23:21:20 +0900

    AsyncSubject

---
+++ Assets/Example/Example27/ObjectA.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+/// <summary>
+/// 먼저 초기화되면 좋은 쪽
+/// </summary>
+public class ObjectA : MonoBehaviour
+{
+    private AsyncSubject<Unit> _initializedAsyncSubject = new AsyncSubject<Unit>();
+
+    public IObservable<Unit> OnInitializedAsync => _initializedAsyncSubject;
+
+    private void Start()
+    {
+        Debug.Log("ObjectA의 Start가 실행되었습니다.");
+
+        // 여기에서 a의 초기화 처리
+
+        Debug.Log("ObjectA의 초기화가 끝났습니다.");
+
+        // 초기화 완료 통지
+        _initializedAsyncSubject.OnNext(Unit.Default);
+        _initializedAsyncSubject.OnCompleted();
+    }
+}
---
+++ Assets/Example/Example27/ObjectB.cs
@@ -0,0 +1,26 @@
+﻿using UniRx;
+using UnityEngine;
+
+/// <summary>
+/// ObjectA 후 초기화 되었으면 하는 쪽
+/// </summary>
+public class ObjectB : MonoBehaviour
+{
+    [SerializeField]
+    private ObjectA objectA;
+
+    private void Start()
+    {
+        Debug.Log("ObjectB의 Start가 실행되었습니다.");
+
+        // A의 초기화 완료 통지가 오면 B를 초기화
+        // OnInitializedAsync 이미 Completed 된 경우 즉시 실행된다.
+        objectA.OnInitializedAsync.Subscribe(_ => Initialize());
+    }
+
+    private void Initialize()
+    {
+        // 여기에서 B의 초기화 처리
+        Debug.Log("ObjectB의 초기화가 끝났습니다.");
+    }
+}
