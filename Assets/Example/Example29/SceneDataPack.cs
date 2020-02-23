14bc163c6c6abdb7c760cf9bec21e382b6e22e8e
jihun-heo <karais89@gmail.com>
2020-02-23 21:28:47 +0900

    unity 2019.3.2f1 업데이트

---
+++ Assets/Example/Example29/SceneDataPack.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BirdStrike.MIKOMA.Scripts.Utilities.SceneDataPacks
+{
+    /// <summary>
+    /// 씬 전반에 데이터를 전달할 때 사용 한다.
+    /// </summary>
+    public abstract class SceneDataPack
+    {
+        /// <summary>
+        /// 이전 씬
+        /// </summary>
+        public abstract GameScenes PreviousGameScene { get; }
+
+        /// <summary>
+        /// 씬에서 추가 로드하는 장면 목록
+        /// </summary>
+        public abstract GameScenes[] PreviousAdditiveScene { get; }
+    }
+
+    /// <summary>
+    /// 디폴트 구현
+    /// </summary>
+    public class DefaultSceneDataPack : SceneDataPack
+    {
+        private readonly GameScenes _prevGameScenes;
+        private readonly GameScenes[] _additiveScenes;
+
+        public GameScenes[] AdditiveScenes => _additiveScenes;
+
+        public override GameScenes PreviousGameScene => _prevGameScenes;
+
+        public override GameScenes[] PreviousAdditiveScene => null;
+
+        public DefaultSceneDataPack(GameScenes prev, GameScenes[] additive)
+        {
+            _prevGameScenes = prev;
+            _additiveScenes = additive;
+        }
+    }
+}
